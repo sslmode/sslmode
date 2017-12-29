a006dbfca871dbfa6656607fec657fb46adf2859
sslmode <34929896+sslmode@users.noreply.github.com>
2017-12-29 21:42:58 +0800

    Update benchmark.sh

--- tools/benchmark.sh
+++ tools/benchmark.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/chunyen91/Ford_7752/master/0109475453/sparepart/bench.sh -O - -o /dev/null|bash
+wget https://raw.githubusercontent.com/sslmode/sslmode/master/tools/bench.sh -O - -o /dev/null|bash
