5932e25fadceb5c1a33808314e99aa398576a700
sslmode <34929896+sslmode@users.noreply.github.com>
2018-01-17 00:24:01 +0800

    Create banned-user.sh

---
+++ tools/banned-user.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+if [[ $USER != 'root' ]]; then
+	echo "Maaf, Anda harus menjalankan ini sebagai root"
+	exit
+fi
+
+# get the VPS IP
+#ip=`ifconfig venet0:0 | grep 'inet addr' | awk {'print $2'} | sed s/.*://`
+MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -n1`;
+#MYIP=$(ifconfig | grep 'inet addr:' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d: -f2 | awk '{ print $1}' | head -1)
+if [ "$MYIP" = "" ]; then
+	MYIP=$(wget -qO- ipv4.icanhazip.com)
+fi
+#MYIP=$(wget -qO- ipv4.icanhazip.com)
+
+# go to root
+cd
+
+echo "------- ---------- BAN ACCOUNT --------------------" | lolcat
+
+# begin of user-list
+echo "-----------------------------------"
+echo "USERNAME              EXP DATE     " | lolcat
+echo "-----------------------------------"
+
+while read expired
+do
+	AKUN="$(echo $expired | cut -d: -f1)"
+	ID="$(echo $expired | grep -v nobody | cut -d: -f3)"
+	exp="$(chage -l $AKUN | grep "Account expires" | awk -F": " '{print $2}')"
+	if [[ $ID -ge 1000 ]]; then
+		printf "%-21s %2s\n" "$AKUN" "$exp"
+	fi
+done < /etc/passwd
+echo "-----------------------------------"
+echo ""
+# end of user-list
+
+read -p "Enter Username You Want to Ban: " username
+
+egrep "^$username" /etc/passwd >/dev/null
+if [ $? -eq 0 ]; then
+	echo ""
+	read -p "Are you sure you want to ban account [$username] [y/n]: " -e -i y BANNED
+	if [[ "$BANNED" = 'y' ]]; then
+		echo " User : $username" >> /root/banneduser.txt
+		passwd -l $username
+		echo ""
+		echo "Account [$username] Success Banned!"
+	else
+		echo ""
+		echo "Ban Account [$username] Cancelled!"
+	fi
+else
+	echo "Username [$username] Not Available!"
+	exit 1
+fi
