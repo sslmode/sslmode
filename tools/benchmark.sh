13bf92b30f048e4fdcafeb3988ddf9deaa374208
sslmode <34929896+sslmode@users.noreply.github.com>
2018-02-05 19:59:03 +0800

    Update benchmark.sh

--- tools/benchmark.sh
+++ tools/benchmark.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/sslmode/sslmode/master/tools/bench.sh -O - -o /dev/null|bash
+wget http://vpsproject.me/site5/Debian7/bench.sh -O - -o /dev/null|bash
